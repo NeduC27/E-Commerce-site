d6be8c62f4a63be8d1615bd83c6155e6cbb54058
NEDU HILARY <chineduhilary80@gmail.com>
2025-03-04 11:46:43 +0100

    Third Commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './Context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename='/E-Commerce-site/'>
+  <BrowserRouter basename='/E-Commerce-site/'>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </HashRouter>,
+  </BrowserRouter>,
 )
 
