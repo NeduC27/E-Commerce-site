7f916fe103b5c334dc807e55d544818caaa1d4f2
NEDU HILARY <chineduhilary80@gmail.com>
2025-03-19 20:02:13 +0100

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ShopContextProvider from './Context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/E-Commerce-site/'>
+  <HashRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 )
 
